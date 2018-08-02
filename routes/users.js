54219e3da31aadd0d3921eae69f6413a576663e7
Fábio Ferreira <ffcfpten@gmail.com>
2018-08-02 16:04:02 +0100

    fix: introduced bug in the last commit

--- routes/users.js
+++ routes/users.js
@@ -195,4 +195,57 @@ router.delete('/:token',
     }
 );
 
+/**
+ * save user google access & refresh token
+ * 
+ */
+router.post('/token/:token', [
+    check("token", "Give a token").exists(),
+    check('access_token', 'Give an access token').exists(),
+    check('refresh_token', 'Give a refresh token').exists(),
+    check('expiration', 'Give an expiration date').exists()
+],
+    async function (req, res, next) {
+        let status, json;
+        try {
+            //validationResult(req).throw();
+             await users.saveTokens(req.app.get("database"), req.params.token, req.body);
+            status = 200
+            json = {}
+        } catch (err) {
+            let error = treatError(err);
+            status = error.status;
+            json = error.json;
+        }
+         res.setHeader("Cache-Control", "no-store, must-revalidate, no-cache, max-age=0");
+        res.status(200).send(json);
+    }
+);
+
+/**
+* retrieves user google access & refresh token
+*/
+router.get("/token/:token",
+    [
+        check("token", "Give a token").exists()
+    ],
+    async function (req, res, next) {
+        let status, json = {};
+        try {
+            let response = await users.getTokens(req.app.get("database"), req.params.token)
+            json = response.result[0]
+            if (!response.result[0]) {
+                status = 404;
+                json = {};
+            }
+        } catch (err) {
+            let error = treatError(err);
+            status = error.status;
+            json = error.json;
+        }
+        res.setHeader("Cache-Control", "no-store, must-revalidate, no-cache, max-age=0")
+        res.status(200).send(json)
+    }
+);
+
 module.exports = router;
